874b0efd2d7901194c9498791dede5134082ee66
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-07-25 17:47:32 +0530

    Minor changes to notifications and tried out a release
    Signed-off-by: Lalit Umbarkar <lalit.umbarkar9@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
 use std::thread::sleep;
 
-use chrono::Local;
+use chrono::{Local, Timelike};
 use cron_tab::Cron;
 use notify_rust::Notification;
 
 fn main() {
-    println!("Started a cronjob at *sec 0min *hour *day *month *dayOfWeek *year");
-
-    send_notification();
-
-    loop {
-        sleep(std::time::Duration::from_secs(10));
-    }
+    println!("Started a cronjob at 0sec *min *hour *day *month *dayOfWeek *year");
 
     // Get current timezone and create a cron, with that timezone
-    /*
     let local_tz = Local::now().timezone();
     let mut minutely_cron = Cron::new(local_tz);
 
     let _job_id = minutely_cron
-        .add_fn("0 * * * * * *", || println!("Current time is {:?}", Local::now()))
+        .add_fn("0 * * * * * *", send_notification)
         .unwrap();
 
     minutely_cron.start();
 
+    // Run an infinite loop so that only killing the
     loop {
         sleep(std::time::Duration::from_secs(10));
     }
-    */
 }
 
 fn send_notification() {
-    let notification = Notification::new()
-        .summary("Its x o'clock")
+    // Get the current time
+    let current_hour = Local::now().hour();
+    let _notification = Notification::new()
+        .summary(&format!("Its {} o'clock", current_hour))
+        // This will auto dismiss the notification
+        .timeout(100)
         .show();
 }
 
